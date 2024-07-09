5ca60b176ced6b3310eb95bfdb0c73009c9099ea
qinisonhlakaniphoxulu <qiniso.nhlakanipho.xulu@gmail.com>
2024-07-09 12:53:50 +0200

    "Committed"

--- products/views.py
+++ products/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Products
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, World!")
+    products = Products.objects.all()
+    return render(request, 'index.html', {'products' : products})
 
 def new(request):
     return HttpResponse("New Created")
